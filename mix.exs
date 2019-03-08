6d26cdeb79ae256ed19401d932988e83a3ec89cd
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-08 23:32:33 +0800

    Add ex_doc to dependency

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule FluminusCLI.MixProject do
       {:fluminus, "~> 0.2.2"},
       {:jason, "~> 1.1"},
       {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
