62eb322747d4316e8f0ce68e8df4177099f98da9
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-08 23:30:17 +0800

    Move CLI from fluminus

---
+++ lib/mix/tasks/fluminus.ex
@@ -0,0 +1,29 @@
+defmodule Mix.Tasks.Fluminus do
+  @help """
+  mix fluminus [OPTIONS]
+
+  --announcements     Show announcements
+  --files             Show files
+  --download-to=PATH  Download files to PATH
+  """
+
+  @moduledoc """
+  Runs the Fluminus CLI.
+
+  ```
+  #{@help}
+  ```
+  """
+
+  @shortdoc "Runs the Fluminus CLI."
+
+  use Mix.Task
+
+  def run(args) do
+    if "--help" in args or "-h" in args do
+      IO.puts(@help)
+    else
+      FluminusCLI.run(args)
+    end
+  end
+end
--- mix.exs
+++ mix.exs
@@ -5,9 +5,23 @@ defmodule FluminusCLI.MixProject do
     [
       app: :fluminus_cli,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/indocomsoft/fluminus_cli",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      package: package(),
+      description: description(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +35,22 @@ defmodule FluminusCLI.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:fluminus, "~> 0.2.2"},
+      {:jason, "~> 1.1"},
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "A CLI client for the reverse-engineered LumiNUS API (https://luminus.nus.edu.sg)"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/indocomsoft/fluminus_cli"}
     ]
   end
 end
