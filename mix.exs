a723f5065adebe0b91cccc5f1b81eb2c5b427ac3
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-08-26 19:04:09 +0800

    Bump version to 0.4.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FluminusCLI.MixProject do
   def project do
     [
       app: :fluminus_cli,
-      version: "0.4.5",
+      version: "0.4.6",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
